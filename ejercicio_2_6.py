54c0b5c63e27db4ee9e20f4691771b8f092b95c7
Jorge Ramón <jorge.ramon2128@gmail.com>
2022-02-10 20:18:30 -0600

    Solución al ejercicio 2.6

---
+++ ejercicio_2_6.py
@@ -0,0 +1,15 @@
+import math
+
+print("Ingresa el valor de A:")
+
+a = float(input())
+
+print("Ingresa el valor de B:")
+
+b = float(input())
+
+c_cuadrada = (a ** 2) + (b ** 2)
+
+c = math.sqrt(c_cuadrada)
+
+print("La hipotenusa es:", c)
